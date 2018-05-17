5f03b4af9b404086ceac78050cf276af2e4bf4bf
Stijn Van Dessel <vandesselstijn@gmail.com>
2018-05-17 07:04:24 +0200

    Mathlab opgave 2

--- Ontwerp_actief_filter/AFcheb.m
+++ Ontwerp_actief_filter/AFcheb.m
@@ -1,17 +1,20 @@
-%Voorbeeld Butterworth filter
+%Stijn Van Dessel
+%Ontwerp Butterworth filter
 %Specificaties
-fc = 1000; %hz
-fs = 3100; %hz
-K  = 2;
-As = 40; %db
-Ap = 3;  %db
+fc = 2000; %hz
+fs = 6200; %hz
+K  = 3;    %3dB
+K   = 10^(K/20);    % dec
+As = 40;   %db
+Ap = 3;    %db
 
-[n,Wn] = buttord(fc*2*pi,fs*2*pi,Ap,As,'s')
+[n,Wn] = cheb1ord(fc*2*pi,fs*2*pi,Ap,As,'s')
 fn = Wn/(2*pi)
 
-[T,N] = butter(n,Wn,'s')
+[T,N] = cheby1(n,Ap,Wn,'s')
 
 filter = tf(T,N)
+filter = K*filter
 
 figure(1);
 bode(filter);
@@ -26,39 +29,36 @@ trappen = zpk(filter)
 
 [wn, zeta] = damp(filter)
 
-zeta_1ste_orde  = zeta(3)
-zeta_2de_orde_1 = zeta(1)
-zeta_2de_orde_2 = zeta(5)
+zeta_1ste_orde  = zeta(1)
+zeta_2de_orde = zeta(2)
 
-wn_1ste_orde  = wn(3)
-wn_2de_orde_1 = wn(1)
-wn_2de_orde_2 = wn(5)
+wn_1ste_orde  = wn(1)
+wn_2de_orde = wn(2)
 
 q_1ste_orde  = 1/(2*zeta_1ste_orde)
-q_2de_orde_1 = 1/(2*zeta_2de_orde_1)
-q_2de_orde_2 = 1/(2*zeta_2de_orde_2)
+q_2de_orde = 1/(2*zeta_2de_orde)
 
 % Bode Diagram
 %      s^2     s^1        s^0
-H_N_1 = 1                                      % numerator
+H_N_1 = 1                           % numerator
 H_D_1 = [(1/wn_1ste_orde) 1]        % denomerator
 
 %      s^2     s^1        s^0
-H_N_2 = K*[0 0 1]                     % numerator
-H_D_2 = [(1/wn_2de_orde_1)^2 ((1/q_2de_orde_1)*(1/wn_2de_orde_1)) 1]        % denomerator
-
-%      s^2     s^1        s^0
-H_N_3 = K*[0 0 1]                     % numerator
-H_D_3 = [(1/wn_2de_orde_2)^2 ((1/q_2de_orde_2)*(1/wn_2de_orde_2)) 1]        % denomerator
-
+H_N_2 = K*[0 0 1]                                                           % numerator
+H_D_2 = [(1/wn_2de_orde)^2 ((1/q_2de_orde)*(1/wn_2de_orde)) 1]        % denomerator
 
 H_1 = tf(H_N_1,H_D_1)
 H_2 = tf(H_N_2,H_D_2)
-H_3 = tf(H_N_3,H_D_3)
 
-figure(4)
+figure(1)
 hold on;
-bode(H_1,opts);
-bode(H_2,opts);
-bode(H_3,opts);
-bode(filter);
+bode(H_1);
+bode(H_2);
+figure(2)
+hold on;
+step(H_1);
+step(H_2);
+figure(3)
+hold on;
+pzmap(H_1);
+pzmap(H_2);
